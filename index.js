43ce5ca54a7ff2d8bbd2270d24393c660a8346e3
jakedub <jacob.w.moore@gmail.com>
2017-08-18 15:05:51 -0400

    small changes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().sp
 //body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressValidator());
 
 //styling
 app.use(express.static("public"));
@@ -28,15 +29,31 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+//variable for storing the randomly assigned word
+const list = [{
+  word: "",
+  incorrect: false;
+}]
+
 //rendering to initial page. Setting home as initial site. Using index.mustache for the page.
 app.get("/home", function (req,res){
   res.render("index");
 })
 
-//assign random word based on the static directory
+//assign random word based on the static directory. // TODO: Need to find a way to assign a word. Then store it.
 app.post("/home", function (req,res){
+  if (originalUrl === "/home"){
+    assign random letter.
+  }
   res.render("index")
 })
+
+
+//get the input, match against the stored word. Using sessions likely
+app.get("/", function (req,res){
+  console.log("checkpoing");
+  res.redirect("/home");
+});
 
 //redirect if count is over 8. // TODO: Needs session count.
 app.get("/", function(req,res){
@@ -47,7 +64,10 @@ app.get("/", function(req,res){
 })
 
 
+
+
+
 //local checking
 app.listen(3000, function(){
-  console.log("Should be working");
+  console.log("I can see you");
 })
